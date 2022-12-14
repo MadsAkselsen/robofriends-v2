4397f95f8e9da9d4b2a9273a7e4155e17708ddce
Mads Akselsen <mads.akselsen@maersk.com>
2022-12-14 11:01:12 +0100

    putting redux to use

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
+import { setSearchField } from '../actions';
 
-function App() {
+//! gotcha:
+// if there is only one reducer added to the store (in index.js),
+// the prop is directly on the state (state.searchField).
+// If multiple reducers are added to the store, the prop must
+// be accessed by specifying which reducer: state.searchRobots.searchField
+const mapStateToProps = (state) => {
+	return {
+		searchField: state.searchField,
+	};
+};
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+	};
+};
+
+function App({ onSearchChange, searchField }) {
 	const [robots, setRobots] = useState([]);
-	const [searchfield, setSearchfield] = useState('');
 	const [count, setCount] = useState(0); // for demo purposes
 
 	useEffect(() => {
@@ -18,12 +36,8 @@ function App() {
 		// console.log(count)
 	}, []); // if you add count, only run if count changes.
 
-	const onSearchChange = (event) => {
-		setSearchfield(event.target.value);
-	};
-
 	const filteredRobots = robots.filter((robot) => {
-		return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+		return robot.name.toLowerCase().includes(searchField.toLowerCase());
 	});
 
 	return !robots.length ? (
@@ -40,4 +54,4 @@ function App() {
 	);
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import "tachyons";
-import App from "./containers/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore } from 'redux';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import App from './containers/App';
+import { searchRobots } from './reducers';
+
+const store = createStore(searchRobots);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
